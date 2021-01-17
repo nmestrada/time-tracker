73fc47c9a765db3eb79cccc71425e0cd8ef52185
Natalie Estrada <nmestrad@gmail.com>
2021-01-17 12:41:47 -0600

    added electron working

---
+++ src/components/TaskList.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+html
+    h2 Task List
+    div.task(v-for='task in tasks')
+        p {{task.name}}
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'Task List',
+  data() {
+    return {
+      tasks: [
+        { id: 1, name: 'task1' },
+        { id: 2, name: 'task2' },
+      ],
+    };
+  },
+});
+</script>
+
+<style scoped>
+.task {
+  color: blue;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <TaskList />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import TaskList from '@/components/TaskList.vue';
 
 export default defineComponent({
   name: 'Home',
   components: {
     HelloWorld,
+    TaskList,
   },
 });
 </script>
